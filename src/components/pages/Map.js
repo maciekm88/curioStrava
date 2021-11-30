81f089d1379566f6b7ffb6e36bead8e67b480c7f
maciekm88 <maciek.m.88@gmail.com>
2021-11-30 17:03:30 +0100

    Getting strava api activities response in Map.js

--- src/components/pages/Map.js
+++ src/components/pages/Map.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, {useEffect} from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import axios from 'axios'
 
 const Map = () => {
+
+    const client_id = "74820";
+    const client_secret = "aa90f8bede45989f7229e964ca147e6bbaa76f4e";
+    const refresh_token = "32d3509503958f6fc781b5ec6ae171ef1435d938";
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities`;
+
+    useEffect(() => {
+        async function fetchData() {
+            const stravaActivityResponse = await axios.get(`${activities_link}?access_token=fecf5cd7ea7eaa08e6103de73d0592e1d8122b2d`);
+            console.log(stravaActivityResponse);
+        }
+            fetchData();
+        }, []);
+
+
+
+
+
+
+
     return (
-        <div className="map">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-        </MapContainer>
-        </div>
+        <h2>testing</h2>
+
     );
 };
 
